10dad2ea1493c959b25737afbfb2940ac14f7933
Becky Burton <rebeccabexb@gmail.com>
2026-03-23 21:09:46 +0000

    Initial frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Welcome to Arcane Cricket</h1>
+          <p>
+            A game where cricket meets magic
+          </p>
+        </div>
+      </section>
+
+    </>
+  )
+}
+
+export default App
