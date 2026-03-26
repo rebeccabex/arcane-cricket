6982ebd2d4d49476799d8061136dc482c1f1ca22
Becky Burton <rebeccabexb@gmail.com>
2026-03-26 04:29:00 +0000

    Added menu options to main page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { apiCall } from './api';
 import './App.css'
+import styled from 'styled-components';
 
 const App = () => {
   const [loadMessage, setLoadMessage] = useState('');
@@ -16,8 +17,8 @@ const App = () => {
 
   return (
     <>
-      <section id="center">
-        <div>
+      <MainPage>
+        <Banner>
           <h1>Welcome to Arcane Cricket League</h1>
           <p>
             Where Cricket meets Magic!
@@ -25,11 +26,45 @@ const App = () => {
           <p>
             {loadMessage}
           </p>
-        </div>
-      </section>
-
+        </Banner>
+        <Menu>
+          <MenuOption disabled>Single Match</MenuOption>
+          <MenuOption disabled>Campaign</MenuOption>
+          <MenuOption disabled>Options</MenuOption>
+        </Menu>
+      </MainPage>
     </>
   )
 }
+
+const MainPage = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  place-content: center;
+  place-items: center;
+  flex-grow: 1;
+`;
+
+const Banner = styled.div`
+  @media (max-width: 1024px) {
+    padding: 32px 20px 24px;
+    gap: 18px;
+  }
+`;
+
+const Menu = styled.ul`
+  list-style-type: none;
+
+  @media (max-width: 1024px) {
+    padding: 32px 20px 24px;
+    gap: 18px;
+  }
+`;
+
+const MenuOption = styled.li<{ disabled?: boolean }>`
+  color: ${props => props.disabled ? "#3e4043" : "#9ca3af" }
+`;
+
 
 export default App
